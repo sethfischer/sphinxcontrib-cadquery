2b4bda875569e8055bfacafc7a6000fa5cc83f1c
Seth Fischer <seth@fischer.nz>
2023-01-13 23:01:50 +1300

    refactor: pep8 naming conventions

--- sphinxcontrib/cadquery/cq_core.py
+++ sphinxcontrib/cadquery/cq_core.py
@@ -16,23 +16,25 @@ import traceback
 from json import dumps
 
 from cadquery import Assembly, Color, Compound, Sketch, cqgi, exporters
-from cadquery.occ_impl.assembly import toJSON
-from cadquery.occ_impl.jupyter_tools import DEFAULT_COLOR
+from cadquery.occ_impl.assembly import toJSON as cq_assembly_toJSON
 from docutils.parsers.rst import Directive, directives
 from jinja2 import Environment, PackageLoader, select_autoescape
 
+DEFAULT_COLOR = [1, 0.8, 0, 1]
 
-class cq_directive(Directive):
+
+class CqSvgDirective(Directive):
+    """CadQuery SVG directive."""
+
     has_content = True
     required_arguments = 0
-    optional_arguments = 2
+    optional_arguments = 1
     option_spec = {
-        "height": directives.length_or_unitless,
-        "width": directives.length_or_percentage_or_unitless,
         "align": directives.unchanged,
     }
 
     def run(self):
+        """Generate SVG render of CadQuery model."""
         options = self.options
         content = self.content
         state_machine = self.state_machine
@@ -43,18 +45,12 @@ class cq_directive(Directive):
             autoescape=select_autoescape(),
         )
 
-        # only consider inline snippets
-        plot_code = "\n".join(content)
-
-        # Since we don't have a filename, use a hash based on the content
-        # the script must define a variable called 'out', which is expected to
-        # be a CQ object
-        out_svg = "Your Script Did not assign call build_output() function!"
+        model_source = "\n".join(content)
 
         try:
-            result = cqgi.parse(plot_code).build()
+            result = cqgi.parse(model_source).build()
             if result.success:
-                out_svg = exporters.getSVG(
+                svg_document = exporters.getSVG(
                     exporters.toCompound(result.first_result.shape)
                 )
             else:
@@ -62,17 +58,13 @@ class cq_directive(Directive):
 
         except Exception:
             traceback.print_exc()
-            out_svg = traceback.format_exc()
-
-        txt_align = "left"
-        if "align" in options:
-            txt_align = options["align"]
+            svg_document = traceback.format_exc()
 
         rst_markup = jinja_env.get_template("svg.rst.jinja").render(
             include_source=env.config.cadquery_include_source,
-            source=plot_code,
-            out_svg=out_svg,
-            txt_align=txt_align,
+            source=model_source,
+            svg_document=svg_document,
+            align=options.get("align", "left"),
         )
 
         state_machine.insert_input(
@@ -82,20 +74,21 @@ class cq_directive(Directive):
         return []
 
 
-class cq_directive_vtk(Directive):
+class CqVtkDirective(Directive):
+    """CadQuery VTK directive."""
 
     has_content = True
     required_arguments = 0
-    optional_arguments = 2
+    optional_arguments = 4
     option_spec = {
-        "height": directives.length_or_unitless,
-        "width": directives.length_or_percentage_or_unitless,
         "align": directives.unchanged,
+        "height": directives.length_or_unitless,
         "select": directives.unchanged,
+        "width": directives.length_or_percentage_or_unitless,
     }
 
     def run(self):
-
+        """Generate VTK render of CadQuery model."""
         options = self.options
         content = self.content
         state_machine = self.state_machine
@@ -106,12 +99,10 @@ class cq_directive_vtk(Directive):
             autoescape=select_autoescape(),
         )
 
-        # only consider inline snippets
-        plot_code = "\n".join(content)
+        model_source = "\n".join(content)
 
-        # collect the result
         try:
-            result = cqgi.parse(plot_code).build()
+            result = cqgi.parse(model_source).build()
 
             if result.success:
                 if result.first_result:
@@ -120,26 +111,26 @@ class cq_directive_vtk(Directive):
                     shape = result.env[options.get("select", "result")]
 
                 if isinstance(shape, Assembly):
-                    assy = shape
+                    assembly = shape
                 elif isinstance(shape, Sketch):
-                    assy = Assembly(shape._faces, color=Color(*DEFAULT_COLOR))
+                    assembly = Assembly(shape._faces, color=Color(*DEFAULT_COLOR))
                 else:
-                    assy = Assembly(shape, color=Color(*DEFAULT_COLOR))
+                    assembly = Assembly(shape, color=Color(*DEFAULT_COLOR))
             else:
                 raise result.exception
 
         except Exception:
             traceback.print_exc()
-            assy = Assembly(Compound.makeText("CQGI error", 10, 5))
+            assembly = Assembly(Compound.makeText("CQGI error", 10, 5))
 
-        data = dumps(toJSON(assy))
+        vtk_json = dumps(cq_assembly_toJSON(assembly))
 
         rst_markup = jinja_env.get_template("vtk.rst.jinja").render(
             include_source=env.config.cadquery_include_source,
-            source=plot_code,
-            data=data,
+            source=model_source,
+            vtk_json=vtk_json,
             element="document.currentScript.parentNode",
-            txt_align=options.get("align", "left"),
+            align=options.get("align", "left"),
             width=options.get("width", "100%"),
             height=options.get("height", "500px"),
         )
