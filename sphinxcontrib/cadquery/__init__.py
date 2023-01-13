71238ce4e3fa7b232a729ee0daa3bf2100f0b3c8
Seth Fischer <seth@fischer.nz>
2023-01-13 23:01:50 +1300

    refactor: pep8 naming conventions

--- sphinxcontrib/cadquery/__init__.py
+++ sphinxcontrib/cadquery/__init__.py
@@ -5,13 +5,13 @@ from pathlib import Path
 
 from sphinx.application import Sphinx
 
-from .cq_core import cq_directive, cq_directive_vtk
+from .cq_core import CqSvgDirective, CqVtkDirective
 
 __version__ = "0.2.1"
 
 _ROOT_DIR = Path(__file__).absolute().parent
 
-_FILES = {
+_JS_FILES = {
     "static/dist/vtk-lite.js": {"priority": 90},
     "static/render.js": {"priority": 100},
 }
@@ -30,7 +30,7 @@ def setup(app: Sphinx):
         app_outdir_dist = app_static_directory / "dist"
         app_outdir_dist.mkdir(parents=True, exist_ok=True)
 
-        for filename, metadata in _FILES.items():
+        for filename, metadata in _JS_FILES.items():
             source = _ROOT_DIR / filename
             destination = app_outdir_dist / Path(filename).name
 
@@ -43,11 +43,11 @@ def setup(app: Sphinx):
 
         app._sphinxcontrib_cadquery_vtkjs_installed = True
 
-    app.add_directive("cadquery-svg", cq_directive)
-    app.add_directive("cadquery-vtk", cq_directive_vtk)
+    app.add_directive("cadquery-svg", CqSvgDirective)
+    app.add_directive("cadquery-vtk", CqVtkDirective)
 
-    app.add_directive("cq_plot", cq_directive)  # deprecated, use cadquery-svg
-    app.add_directive("cadquery", cq_directive_vtk)  # deprecated, use cadquery-vtk
+    app.add_directive("cq_plot", CqSvgDirective)  # deprecated, use cadquery-svg
+    app.add_directive("cadquery", CqVtkDirective)  # deprecated, use cadquery-vtk
 
     app.add_config_value("cadquery_include_source", True, "env")
 
